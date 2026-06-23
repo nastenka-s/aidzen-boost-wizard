4ee0d24238b4c0d5baf4be3cb43e6636983f55a0
nastenka-s <applatonova@gmail.com>
2026-06-23 09:14:12 +0300

    feat(blog): auto-publish "Число имени в нумерологии: ваш код личности и талантов"

--- src/App.tsx
+++ src/App.tsx
@@ -59,6 +59,7 @@ import HironVNatalnojKarte from "./pages/blog-auto/HironVNatalnojKarte";
 import ProgressiiVAstrologii from "./pages/blog-auto/ProgressiiVAstrologii";
 import VozvrashchenieSaturna2930Let from "./pages/blog-auto/VozvrashchenieSaturna2930Let";
 import ChisloDushiPoDateRozhdeniya from "./pages/blog-auto/ChisloDushiPoDateRozhdeniya";
+import ChisloImeniNumerologiya from "./pages/blog-auto/ChisloImeniNumerologiya";
 // __AUTOGEN_IMPORTS__ (do not remove — used by generate-blog-post edge function)
 import CookieConsent from "./components/CookieConsent";
 
@@ -75,6 +76,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/blog" element={<Blog />} />
           {/* __AUTOGEN_ROUTES__ (do not remove — used by generate-blog-post edge function) */}
+          <Route path="/chislo-imeni-numerologiya" element={<ChisloImeniNumerologiya />} />
           <Route path="/chislo-dushi-po-date-rozhdeniya" element={<ChisloDushiPoDateRozhdeniya />} />
           <Route path="/vozvrashchenie-saturna-29-30-let" element={<VozvrashchenieSaturna2930Let />} />
           <Route path="/progressii-v-astrologii" element={<ProgressiiVAstrologii />} />
