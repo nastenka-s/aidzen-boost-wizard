057cec91bf7279fe8ac888330142cfd42206be23
nastenka-s <applatonova@gmail.com>
2026-08-02 09:01:40 +0300

    feat(blog): auto-publish "Финансовый поток в матрице судьбы: как открыть денежный кана"

--- src/App.tsx
+++ src/App.tsx
@@ -102,6 +102,7 @@ import LunnyjKalendarSvadeb2026 from "./pages/blog-auto/LunnyjKalendarSvadeb2026
 import KarmicheskayaSovmestimost from "./pages/blog-auto/KarmicheskayaSovmestimost";
 import MasterChislaNumerologiya from "./pages/blog-auto/MasterChislaNumerologiya";
 import NumerologiyaNomeraTelefona from "./pages/blog-auto/NumerologiyaNomeraTelefona";
+import FinansovyjPotokMatrica from "./pages/blog-auto/FinansovyjPotokMatrica";
 // __AUTOGEN_IMPORTS__ (do not remove — used by generate-blog-post edge function)
 import CookieConsent from "./components/CookieConsent";
 
@@ -118,6 +119,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/blog" element={<Blog />} />
           {/* __AUTOGEN_ROUTES__ (do not remove — used by generate-blog-post edge function) */}
+          <Route path="/finansovyj-potok-matrica" element={<FinansovyjPotokMatrica />} />
           <Route path="/numerologiya-nomera-telefona" element={<NumerologiyaNomeraTelefona />} />
           <Route path="/master-chisla-numerologiya" element={<MasterChislaNumerologiya />} />
           <Route path="/karmicheskaya-sovmestimost" element={<KarmicheskayaSovmestimost />} />
